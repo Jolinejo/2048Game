0a15d43a1af0b5c081a0e21bd6b61298f84144a6
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-09-07 17:21:31 +0300

    Initial Commit

---
+++ 2048Game/_048GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  _048GameApp.swift
+//  2048Game
+//
+//  Created by Maryiam Ajlan on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct _048GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
